6f0b6edd6788ca95eb2b97ea58e9971d7136e412
Abdullah <abdullahvora136@gmail.com>
2025-01-21 22:27:47 +0530

    set up cors

--- server.js
+++ server.js
@@ -7,11 +7,26 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json()); 
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
-app.use(cors());
-app.use(router); 
+app.use(router);
+
+const corsOptions = {
+    origin: [
+        'http://localhost:5173', // Local React App (dev)
+        'http://localhost:5174', // Local React App (dev)
+        'http://localhost:3000',  // Another Local React App (dev)
+        'https://furn-store-xi.vercel.app',
+        'https://e-commerce-admin-beta-ten.vercel.app'
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+    credentials: true, // Allow cookies or authorization headers (if needed)
+};
+
+app.use(cors(corsOptions));  // Apply the CORS middleware
+app.options('*', cors(corsOptions)); // Handle preflight requests
 
 app.listen(PORT, (err) => {
     if (err) throw err;
